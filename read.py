80aed8bce323b434ed83ce4a0141acfb9bde4581
Alan Tu <tu19970201@gmail.com>
2022-04-13 20:58:18 +0800

    cal avr of reviews

--- read.py
+++ read.py
@@ -7,4 +7,10 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
+
+sum_len = 0
+for d in data:        
+        sum_len += len(d) 
+        
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+print('留言平均長度為', sum_len / len(data))
